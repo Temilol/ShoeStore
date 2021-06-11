510a3b4fcf759e674b0ccc3ae74bfc78181f76af
Temilola <temz4rill@gmail.com>
2021-06-10 20:13:47 -0400

    Initial commit

---
+++ app/src/main/java/com/temilol/shoestore/fragment/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.temilol.shoestore.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.temilol.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment: Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+}
